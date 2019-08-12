29d339a5538740e158d5114510bad67a826af8be
Peter Tran <iamatran@hotmail.com>
2019-08-11 23:34:53 -0500

    Full Stack Works!

---
+++ ClientApp/src/app/models/food.model.ts
@@ -0,0 +1,13 @@
+import { Address } from './address.model';
+import { Rating } from './rating.model';
+
+export class Food {
+	constructor(
+		public foodId?: number,
+		public name?: string,
+		public category?: string,
+		public description?: string,
+		public price?: number,
+		public address?: Address,
+		public ratings?: Rating[]) { }
+}
